a3e06c2677881711797d2fad4e0b6634e03d77c4
ManosKout <manos.koutoulak@gmail.com>
2021-12-17 19:13:34 +0100

    workflow - step and dependencies

--- executor.py
+++ executor.py
@@ -3,6 +3,8 @@ import os
 import json
 import logging
 import networkx as nx
+from CWLExecutor impoer CWLExecutor
+
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -47,8 +49,48 @@ class Workflow:
     '''
 
     def __init__(self, workflow_file=None, input_variables=None):
-        workflow = retrieve_workflow(workflow_file)
-        print(workflow)
+        self.workflow = retrieve_workflow(workflow_file)
+        self.parse_workflow()
+
+    def global_env_var(self,):
+        return self.workflow["environment_variables"]
+
+    def get_wf_info(self,):
+        log_info("Workflow steps: ")
+        log_info(self.wf_steps)
+        log_info("Workflow dependencies: ")
+        for item in self.step_dependencies:
+            print(f"{item}")
+        # log_info(self.step_dependencies)
+
+    def get_steps(self,):
+        '''
+        Return the steps of the workflow
+        '''
+        return self.workflow["steps"].keys()
+
+    def get_step_generator(self):
+        """
+        Return a dict that the key is the step that depends on its value.
+        Also there are two more keys (main,final) that are the first and 
+        the final respectively
+        """
+        for step in self.wf_steps:
+            yield (step, self.workflow["steps"][step]["run_after"], self.workflow["steps"][step]["type"])
+
+    def parse_workflow(self):
+        '''
+        Parse the workflow by identifying the steps, dependencies
+        environment variables etc
+        '''
+        if self.workflow:
+            self.wf_steps = self.get_steps()
+            self.step_dependencies = self.get_step_generator()
+        else:
+            self.wf_steps = None
+            self.step_dependencies = None
+            raise JSON_CWLParserException(f"The workflow does not exist. Workflow : {self.workflow}")
+        self.get_wf_info()
 
 
 if __name__ == '__main__':
@@ -81,16 +123,16 @@ if __name__ == '__main__':
     )
     # Future work
     # parser.add_argument(
-    # 	'-G',
-    # 	'--graph',
-    # 	dest='graph',
-    # 	help="The graph of the workflow",
-    # 	default='graph'
-    # 	)
+    #   '-G',
+    #   '--graph',
+    #   dest='graph',
+    #   help="The graph of the workflow",
+    #   default='graph'
+    #   )
 
     args = parser.parse_args()
 
-    print(f"The args that we get: {args}")
+    # print(f"The args that we get: {args}")
 
     workflow = Workflow(
         workflow_file=args.workflow_filename, input_variables=None)
